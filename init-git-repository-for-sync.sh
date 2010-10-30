2e2d408ce52bb64af227972cd1e40f902ecf4a47
Kouhei Sutou <kou@clear-code.com>
2010-10-30 21:47:49 +0900

    make workable.

--- init-git-repository-for-sync.sh
+++ init-git-repository-for-sync.sh
@@ -12,10 +12,8 @@ run()
 ruby_gnome2_repos=https://ruby-gnome2.svn.sourceforge.net/svnroot/ruby-gnome2
 sf_jp_ruby_gnome2_home=/home/groups/r/ru/ruby-gnome2
 
-run git svn clone -s $ruby_gnome2_repos/doc/ ruby-gnome2-doc.git
-run cd ruby-gnome2-doc.git
 run git remote add sf.jp ssh://kous@shell.sourceforge.jp$sf_jp_ruby_gnome2_home/data/hiki-repos
-run git confg --set remote.sf.jp.uploadpack $sf_jp_ruby_gnome2_home/local/bin/git-upload-pack
-run git confg --set remote.sf.jp.receivepack $sf_jp_ruby_gnome2_home/local/bin/git-receive-pack
-run git fetch
+run git config --path remote.sf.jp.uploadpack $sf_jp_ruby_gnome2_home/local/bin/git-upload-pack
+run git config --path remote.sf.jp.receivepack $sf_jp_ruby_gnome2_home/local/bin/git-receive-pack
+run git fetch sf.jp
 run git branch hiki sf.jp/master
